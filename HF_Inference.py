b91238132a0e1f10593da3b77518be682ced9bba
Nagarjun S <nagarjun9689@gmail.com>
2024-06-27 21:06:30 +0530

    completed and included vector stores and retrievers

---
+++ HF_Inference.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from huggingface_hub import InferenceClient
+import json
+
+
+repo_id = "microsoft/Phi-3-mini-4k-instruct"
+
+llm_client = InferenceClient(
+    model=repo_id,
+    timeout=120,
+)
+
+def call_llm(inference_client: InferenceClient, prompt: str):
+    response = inference_client.post(
+        json={
+            "inputs": prompt,
+            "parameters": {"max_new_tokens": 50},
+            "task": "text-generation",
+        },
+    )
+    return json.loads(response.decode())[0]["generated_text"]
+
+response=call_llm(llm_client, "Capital of USA is ")
+print (response)
